bc1adcb1518468dadb0dd70c2b16988c4e75f31f
Gonzalo Ilhuicatzi <g.ilhuicatzi@gmail.com>
2025-02-11 01:04:55 -0600

    Update global styles, add Recharts dependency, and integrate HeroSection and FeatureSection into the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import HeroSection from "@/components/HomePage/HeroSection";
+import FeatureSection from "@/components/HomePage/FeatureSection";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main>
-        <h1>
-          Home Page
-        </h1>
-      </main>
-    </div>
+    <main className="flex flex-col items-start p-8 pb-20 gap-16 sm:p-20">
+      <HeroSection />
+      <FeatureSection />
+    </main>
   );
 }
