3f3246b16f0b8e5265233c495dc87243e6928463
Gonzalo Ilhuicatzi <g.ilhuicatzi@gmail.com>
2025-02-12 23:33:03 -0600

    Refactor LoginPage to integrate LoginComponent and improve layout

--- src/app/public/login/page.tsx
+++ src/app/public/login/page.tsx
@@ -1,12 +1,12 @@
+import { LoginComponent } from "@/components/public/LoginComponent";
+
 export default function LoginPage() {
     return (
-      <div className="flex flex-col items-center justify-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main>
-          <h1>
-            Login Page
-          </h1>
-        </main>
+      <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginComponent />
       </div>
+    </div>
     );
   }
   
